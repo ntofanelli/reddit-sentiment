1e6f4385d05e8c759662aba319bb8896c94f21dc
landryclarke04 <landry.clarke04@gmail.com>
2026-08-30 15:37:26 -0600

    Try another solution for character limit based on tokens.

--- sentiment.py
+++ sentiment.py
@@ -15,12 +15,12 @@ def analyze_sentiment(text):
     # gets label back to integer
     label_to_id = classifier.model.config.label2id
 
-    # cutting off charater length if too long for classifier
-    if len(text) > 2000:
-        text = text[:2000]
+    # cutting off character length if too long for classifier
+    # if len(text) > 2000:
+    #     text = text[:2000]
 
     # analysis
-    result = classifier(text)
+    result = classifier(text, truncation=True, max_length=512)
     
     label = result[0]['label']
     
